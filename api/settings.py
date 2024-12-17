306c5344602d3b8dbb22661b32993c5f66a57e1c
mrbadri <mrbadri.dev@gmail.com>
2024-12-17 21:39:53 +0330

    print

--- api/settings.py
+++ api/settings.py
@@ -126,3 +126,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+print("--------------------------------------------------------")
+print("--------------------------------------------------------")
+print("--------------------------------------------------------")
+print(STATIC_ROOT)
+print(os.path.join(BASE_DIR, 'static'))
