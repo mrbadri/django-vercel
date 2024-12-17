8232366020df31e597f578704490da54758f6bd7
mrbadri <mrbadri.dev@gmail.com>
2024-12-17 22:34:23 +0330

    change vercel json

--- api/settings.py
+++ api/settings.py
@@ -132,8 +132,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # STATIC_URL = './static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
-STATIC_URL =  "./static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 print("--------------------------------------------------------")
 print("--------------------------------------------------------")
